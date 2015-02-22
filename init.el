27bdd457a268c06eeb3afc8ae7ad39d74e8b2efb
Devrim Baris Acar <devrimbaris@yahoo.com>
2015-02-22 11:21:20 +0200

    *** empty log message ***

--- init.el
+++ init.el
@@ -38,26 +38,17 @@
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-
 (setq help-at-pt-display-when-idle t)
 (setq help-at-pt-timer-delay 0.1)
 (help-at-pt-set-timer)
 
 (load-theme 'deeper-blue)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
     ("cbef37d6304f12fb789f5d80c2b75ea01465e41073c30341dc84c6c0d1eb611d" default))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
 
 
